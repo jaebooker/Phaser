0146140250b0542b9b0232f0777addc69669ab43
jaebooker <jaesonbooker@gmail.com>
2019-07-14 15:53:39 -0700

    gamescene

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,7 +1,23 @@
 import { Scene } from 'phaser'
 
 class GameScene extends Scene {
-    constructor() {
-        
+
+    preload(){
+        this.load.image('logo', 'assets/logo.png');
+    }
+
+    create(){
+        var logo = this.add.image(400, 150, 'logo');
+
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: 'Power2',
+            yoyo: true,
+            loop: -1
+        });
     }
 }
+
+export default GameScene
